3f64bd52f0559a2563cb6344319da5ea5fa52a4a
linszay <lindseyjlancaster@gmail.com>
2024-01-09 02:51:13 -0600

    files for task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader', 'image-webpack-loader'],
+        }],
+    },
+};
